956535bc311dc884d432238165fb42d953d49d66
Maximiliano <maximilianovalle@users.noreply.github.com>
2025-08-02 18:04:23 -0500

    Initialize project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WidgetForm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
